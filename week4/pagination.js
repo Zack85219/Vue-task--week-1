0d5b2f7d533870d8f811196b6a7f238d053a3b6d
Zack Chou <j33241796@gmail.com>
2023-01-16 18:15:01 +0800

    week4 start2

--- week4/pagination.js
+++ week4/pagination.js
@@ -1,4 +1,23 @@
 export default {
   props: ["page"],
-  template: `<p>I'm a shadow</p>`,
+  template: `
+  <div class="container">
+  <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item disabled">
+      <a class="page-link" href="#" aria-label="Previous" aria-disabled="true">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+</div>`,
 };
